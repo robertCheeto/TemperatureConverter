847381453ca4593a37629bf810f65e336b933892
Daniel Spezialetti <dspezialetti@my.yearupunited.org>
2025-09-24 10:43:54 -0400

    * changed conversion values to account for doubles * added Celsius conversion

--- src/main/java/com/pluralsight/TemperatureConverter.java
+++ src/main/java/com/pluralsight/TemperatureConverter.java
@@ -10,8 +10,15 @@ public class TemperatureConverter {
         double kelvin;
 
         System.out.println("Converting " + startTempCelsius + " to Fahrenheit...");
-        fahrenheit = (startTempCelsius * (9/5)) + 32;
+        fahrenheit = ((startTempCelsius * (9.0/5.0)) + 32.0);
+        // referenced this to get rounded decimal value - https://stackoverflow.com/questions/11701399/round-up-to-2-decimal-places-in-java
         System.out.println(startTempCelsius + " is: " + Math.round(fahrenheit * 100.0) / 100.0);
+        System.out.println();
+
+        System.out.println("Converting " + startTempFahrenheit + " to Celsius...");
+        celsius = ((startTempFahrenheit - 32.0) * (5.0/9.0));
+        System.out.println(startTempFahrenheit + " is: " + Math.round(celsius * 100.0) / 100.0);
+        System.out.println();
 
     }
 }
