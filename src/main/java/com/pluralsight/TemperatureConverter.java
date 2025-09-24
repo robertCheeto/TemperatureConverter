7cf989c1ad7b70f3b9aff61c3e49577462fab353
Daniel Spezialetti <dspezialetti@my.yearupunited.org>
2025-09-24 11:01:22 -0400

    * added Kelvin conversions * started working on precision sections for each temperature output

--- src/main/java/com/pluralsight/TemperatureConverter.java
+++ src/main/java/com/pluralsight/TemperatureConverter.java
@@ -1,5 +1,7 @@
 package com.pluralsight;
 
+import com.sun.security.jgss.GSSUtil;
+
 public class TemperatureConverter {
     public static void main(String[] args) {
         double startTempCelsius = 23.7;
@@ -12,12 +14,23 @@ public class TemperatureConverter {
         System.out.println("Converting " + startTempCelsius + " to Fahrenheit...");
         fahrenheit = ((startTempCelsius * (9.0/5.0)) + 32.0);
         // referenced this to get rounded decimal value - https://stackoverflow.com/questions/11701399/round-up-to-2-decimal-places-in-java
-        System.out.println(startTempCelsius + " is: " + Math.round(fahrenheit * 100.0) / 100.0);
+        System.out.println(startTempCelsius + " is: " + fahrenheit);
+        System.out.println("Precise temperatures: " + (Math.round(fahrenheit * 100) / 100) + " and " + (Math.round(fahrenheit * 100.0) / 100.0));
         System.out.println();
 
         System.out.println("Converting " + startTempFahrenheit + " to Celsius...");
         celsius = ((startTempFahrenheit - 32.0) * (5.0/9.0));
         System.out.println(startTempFahrenheit + " is: " + Math.round(celsius * 100.0) / 100.0);
+        System.out.println();
+
+        System.out.println("Converting " + startTempCelsius + " to Kelvin...");
+        kelvin = startTempCelsius + 273.15;
+        System.out.println(startTempCelsius + " is: " + Math.round(kelvin * 100.0) / 100.0);
+        System.out.println();
+
+        System.out.println("Converting " + startTempKelvin + " to Celsius...");
+        celsius = startTempKelvin - 273.15;
+        System.out.println(startTempKelvin + " is: " + Math.round(celsius * 100.0) / 100.0);
         System.out.println();
 
     }
